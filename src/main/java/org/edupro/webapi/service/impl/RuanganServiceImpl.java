adf6702017e72ac97186ad34337ce9b381ba1552
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-04-15 17:22:39 +0700

    feat: fix ruangan

--- src/main/java/org/edupro/webapi/service/impl/RuanganServiceImpl.java
+++ src/main/java/org/edupro/webapi/service/impl/RuanganServiceImpl.java
@@ -19,10 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -56,6 +53,7 @@ public class RuanganServiceImpl implements RuanganService {
         }
 
         RuanganEntity result = this.convertReqToEntity(request);
+        result.setId(UUID.randomUUID().toString().toUpperCase());
         return saveOrUpdate(result);
     }
 
