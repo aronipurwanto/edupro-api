e7768abd62d13ef518f6f43a4482f66dd570a751
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-05-04 07:52:36 +0700

    feat: Fix Security

--- src/main/java/org/edupro/webapi/config/SecurityConfiguration.java
+++ src/main/java/org/edupro/webapi/config/SecurityConfiguration.java
@@ -3,18 +3,19 @@ package org.edupro.webapi.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -30,22 +31,20 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //http.cors(Customizer.withDefaults());
+        http.cors(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(auth -> auth
                 // Allows preflight requests from browser
-                .requestMatchers(new AntPathRequestMatcher("/customers*", HttpMethod.OPTIONS.name()))
-                .permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/customers*","/users*"))
-                .hasRole("user")
-                .requestMatchers(new AntPathRequestMatcher("/"))
-                .permitAll()
-                .anyRequest()
-                .authenticated());
+                //.requestMatchers(new AntPathRequestMatcher("/customers*", HttpMethod.OPTIONS.name())).permitAll()
+                //.requestMatchers(new AntPathRequestMatcher("/customers*","/users*")).hasRole("user")
+                //.requestMatchers(new AntPa thRequestMatcher("/")).permitAll()
+                .anyRequest().authenticated());
+        //http.oauth2Login(Customizer.withDefaults()).logout(logout -> logout.addLogoutHandler(keycloakLogoutHandler)
+        //         .logoutUrl("/logout").logoutSuccessUrl("/"));
+        http.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
         http.oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(Customizer.withDefaults()));
-        http.oauth2Login(Customizer.withDefaults())
-                .logout(logout -> logout.addLogoutHandler(keycloakLogoutHandler)
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/"));
         return http.build();
     }
 
